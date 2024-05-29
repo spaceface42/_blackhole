e1d61a4f788c87d3bfe47f6d12f6606c075e3d3b
Zsolt Sándor <ezsolt@gmail.com>
2024-05-29 14:52:12 +0200

    route test

--- docs/rsrc/app/app.js
+++ docs/rsrc/app/app.js
@@ -61,20 +61,23 @@ function initializeUX() {
 
 console.log('fsck');
 
-    window.router = new Router({
-      type: "history",
-      routes: {
-        "/": "home",
-        "/about": "about",
-        "/products": "products"
-      }
-    })
-      .listen()
-      .on("route", (e) => {
-        console.log("Route active:", e.detail.route, "URL: ", e.detail.url);
-    
-        document.getElementById("route").innerHTML = e.detail.route;
-      });
+
+
+
+window.router = new Router({
+    type: "history",
+    routes: {
+    "/": "home",
+    "/about": "about",
+    "/products/:id/detail": "detail"
+    }
+})
+    .listen()
+    .on("route", (e) => {
+    console.log("Route active:", e.detail.route, "URL: ", e.detail.url);
+
+    document.getElementById("route").innerHTML = e.detail.route;
+    });
 
 
 
