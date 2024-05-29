570e8ea3cd6e7dbc0660eb5966b8eb571856626f
Zsolt Sándor <ezsolt@gmail.com>
2024-05-29 12:18:26 +0200

    x

--- docs/rsrc/app/app.js
+++ docs/rsrc/app/app.js
@@ -67,17 +67,15 @@ function initializeUX() {
         routes: {
           "/": "hello world!",
           "/about": "about",
-          "/products": "products",
-          "/products/:id/details": "details"
+          "/products": "products"
         }
       })
         .listen()
         .on("route", (e) => {
-          console.log("Route active:", e.detail.route, "URL:", e.detail.url, "Params:", e.detail.params);
-          document.getElementById("route").innerHTML = `${e.detail.route} ${JSON.stringify(e.detail.params)}`;
+          console.log("Route active:", e.detail.route, "URL:", e.detail.url);
+          document.getElementById("route").innerHTML = e.detail.route;
         });
       
-
       
 
 
